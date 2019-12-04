5eca8c5781c86cf66d67b48abed68dd19bfb9645
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2019-12-04 18:37:01 -0500

    Adding new test method with comments

--- lib/tests/userTests.test.ts
+++ lib/tests/userTests.test.ts
@@ -3,7 +3,7 @@ import { CustomValidators } from "../util/customValidators";
 const customValidators = new CustomValidators() 
 
 describe('Sample Test11222', () => {
-    it('should test that true === true', () => {
+    it('This test should be true!', () => {
         const result = customValidators.suma(3 , 4);
         expect(result).toBe(7)
     })
