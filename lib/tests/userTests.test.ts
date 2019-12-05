ca3412ebe01e682ed0520db89484d706cdf25600
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2019-12-04 20:57:17 -0500

    [BUG] GG-02

--- lib/tests/userTests.test.ts
+++ lib/tests/userTests.test.ts
@@ -2,7 +2,7 @@ import { CustomValidators } from "../util/customValidators";
 
 const customValidators = new CustomValidators() 
 
-describe('Sample Test', () => {
+describe('Sample Test1111111', () => {
     it('should test that true === true', () => {
         const result = customValidators.suma(5 , 4);
         expect(result).toBe(7)
