902eaaa2ee5541667fc3c1a7f1c4955228ab6c23
Ricardo Vásquez <ricardo.vasquez.pe@gmail.com>
2019-12-04 20:59:12 -0500

    [TA-3] Test methods (#9)

--- lib/tests/userTests.test.ts
+++ lib/tests/userTests.test.ts
@@ -3,7 +3,7 @@ import { CustomValidators } from "../util/customValidators";
 const customValidators = new CustomValidators() 
 
 describe('Sample Test', () => {
-    it('should test that true === true', () => {
+    it('should test that true === true22222', () => {
         const result = customValidators.suma(5 , 4);
         expect(result).toBe(7)
     })
