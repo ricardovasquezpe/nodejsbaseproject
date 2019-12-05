6722f9a68d6559bf86ba7801d02cd9519311a8fd
Ricardo Vásquez <ricardo.vasquez.pe@gmail.com>
2019-12-04 20:47:48 -0500

    Staging (#6)
    * Merge from feature/TA-1 to development
    
    * Adding new test method
    
    * Adding new test method with comments
    
    * Merged fixed (#5)

--- lib/tests/userTests.test.ts
+++ lib/tests/userTests.test.ts
@@ -2,7 +2,7 @@ import { CustomValidators } from "../util/customValidators";
 
 const customValidators = new CustomValidators() 
 
-describe('Sample Test', () => {
+describe('Sample Test11222', () => {
     it('should test that true === true 111', () => {
         const result = customValidators.suma(5 , 4);
         expect(result).toBe(7)
