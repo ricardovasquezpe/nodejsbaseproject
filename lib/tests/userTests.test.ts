10c0c64212990abc096fc1f507d28f520c7790ac
Ricardo Vasquez <ricardo.vasquez.pe@gmail.com>
2019-12-04 19:57:13 -0500

    Fix

--- lib/tests/userTests.test.ts
+++ lib/tests/userTests.test.ts
@@ -3,8 +3,8 @@ import { CustomValidators } from "../util/customValidators";
 const customValidators = new CustomValidators() 
 
 describe('Sample Test', () => {
-    it('should test that true === true', () => {
-        const result = customValidators.suma(3 , 4);
+    it('should test that true === true 111', () => {
+        const result = customValidators.suma(5 , 4);
         expect(result).toBe(7)
     })
 })  
